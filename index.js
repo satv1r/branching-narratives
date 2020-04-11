8e812ef3030ba8aa5c883c49d7745a7dea17a288
Satvir Sandhu <356Sandhu@gmail.com>
2020-04-10 22:22:40 -0400

    add children to rootnode

--- index.js
+++ index.js
@@ -26,3 +26,21 @@ class Node {
     });
   }
 }
+
+let rootNode = new Node(null, "Episode 1", "What will happen today?");
+
+rootNode.addNode(
+  "go for a walk",
+  "Episode 1A",
+  "Our character went for a walk today."
+);
+rootNode.addNode(
+  "watch tv",
+  "Episode 1B",
+  "Our character stayed home and watched tv today."
+);
+rootNode.addNode(
+  "do some chores",
+  "Episode 1C",
+  "Our character completed a whole bunch of chores today."
+);
