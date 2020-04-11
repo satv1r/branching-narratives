fc85ed999ad604f2aec845340065335f504d0101
Satvir Sandhu <356Sandhu@gmail.com>
2020-04-11 13:51:50 -0400

    reformat class to module and set testing

---
+++ Node.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(prompt, title, scene) {
+    this.prompt = prompt;
+    this.title = title;
+    this.scene = scene;
+    this.children = [];
+    this.numChildren = 0;
+  }
+
+  addNode(prompt, title, scene) {
+    this.children.push(new Node(title, prompt, scene));
+    this.numChildren++;
+  }
+
+  printContent() {
+    console.log("-----------------------------------------");
+    console.log("Printing contents of current Node");
+    console.log(`Prompt: ${this.prompt}`);
+    console.log(`Title: ${this.title}`);
+    console.log(`Scene: ${this.scene}`);
+    console.log(`Children: ${this.numChildren}`);
+  }
+
+  printChildren() {
+    console.log("-----------------------------------------");
+    this.children.forEach((i, j) => {
+      console.log(`${j}: ${i.prompt}`);
+    });
+  }
+}
+
+module.exports = Node;
--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-class Node {
-  constructor(prompt, title, scene) {
-    this.prompt = prompt;
-    this.title = title;
-    this.scene = scene;
-    this.children = [];
-    this.numChildren = 0;
-  }
-
-  addNode(prompt, title, scene) {
-    this.children.push(new Node(title, prompt, scene));
-    this.numChildren++;
-  }
-
-  printContent() {
-    console.log("-----------------------------------------");
-    console.log("Printing contents of current Node");
-    console.log(`Prompt: ${this.prompt}`);
-    console.log(`Title: ${this.title}`);
-    console.log(`Scene: ${this.scene}`);
-    console.log(`Children: ${this.numChildren}`);
-  }
-
-  printChildren() {
-    console.log("-----------------------------------------");
-    this.children.forEach((i, j) => {
-      console.log(`${j}: ${i.prompt}`);
-    });
-  }
-}
+const Node = require("./Node");
 
 let rootNode = new Node(null, "Episode 1", "What will happen today?");
 
